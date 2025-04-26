d713672b202031b0f080d7a367a53622b64f7221
Alexander Åberg <alexander_aberg1985@msn.com>
2025-04-26 18:49:21 +0200

    Created API project and imported the database

---
+++ BankBlazorAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BankBlazorAPI.Data
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
