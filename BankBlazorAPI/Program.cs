15c467ac5169b5deb82bd7ee24c8d93d9ea1f210
Alexander Åberg <alexander_aberg1985@msn.com>
2025-04-26 22:03:57 +0200

    Updated Program.cs to make sure that the database connection still works

--- BankBlazorAPI/Program.cs
+++ BankBlazorAPI/Program.cs
@@ -1,23 +1,19 @@
-using BankBlazorAPI.Data;
+using BankBlazor.Api.Data.Contexts;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Retrieve the connection string from environment variables
+var connectionString = builder.Configuration.GetConnectionString("BankAppData");
+builder.Services.AddDbContext<BankAppDataContext>(options =>
+    options.UseSqlServer(connectionString));
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-// Secret configuration
-builder.Configuration.AddUserSecrets<Program>();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
