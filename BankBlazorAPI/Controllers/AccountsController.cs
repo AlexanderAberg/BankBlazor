2c0dd85944e4e30a39e50d2cbdb9d935d39b9dad
Alexander Åberg <alexander_aberg1985@msn.com>
2025-04-30 22:06:23 +0200

    Added AccountController with code

---
+++ BankBlazorAPI/Controllers/AccountsController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BankBlazorAPI.Data.Entitites;
+using BankBlazor.Api.Data.Contexts;
+
+namespace BankBlazorAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AccountsController : ControllerBase
+    {
+        private readonly BankAppDataContext _context;
+
+        public AccountsController(BankAppDataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AccountDto>>> GetAccounts()
+        {
+            var accounts = await _context.Accounts
+                .Select(a => new AccountDto
+                {
+                    AccountId = a.AccountId,
+                    Frequency = a.Frequency,
+                    Balance = a.Balance
+                })
+                .ToListAsync();
+
+            return Ok(accounts);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AccountDto>> GetAccount(int id)
+        {
+            var account = await _context.Accounts
+                .Where(a => a.AccountId == id)
+                .Select(a => new AccountDto
+                {
+                    AccountId = a.AccountId,
+                    Frequency = a.Frequency,
+                    Balance = a.Balance
+                })
+                .FirstOrDefaultAsync();
+
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(account);
+        }
+    }
+}
+   
+
